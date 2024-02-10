855a1f277c7698c29e6c8d765410a411f44fe449
Syed Fasiuddin <syedfasiuddin.02@gmail.com>
2024-02-10 20:48:31 +0530

    feat: list of directories of socket files

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
-#![allow(unused_variables, unused_assignments)]
+#![allow(unused_variables, unused_assignments, dead_code)]
 
 use std::env;
+use std::path::Path;
 use std::process::ExitCode;
 
 macro_rules! CTRL {
     ( $ch:expr ) => {
         $ch as u8 & 0x1F
-    }
+    };
 }
 
 const ABDUCO_CMD: &'static str = "dvtm";
+
+struct Dir {
+    path: Box<Path>,
+    personal: bool,
+}
+
+impl Dir {
+    fn form_env(env: &str, personal: bool) -> Self {
+        let path = env::var(env).unwrap_or("".to_string());
+        Self {
+            path: Path::new(&path).into(),
+            personal,
+        }
+    }
+
+    fn from_path(path: &str, personal: bool) -> Self {
+        Self {
+            path: Path::new(&path).into(),
+            personal,
+        }
+    }
+}
 
 fn usage() -> ExitCode {
     println!("usage: abduco [-a|-A|-c|-n] [-r] [-l] [-f] [-e detachkey] name command");
     ExitCode::FAILURE
 }
 
-fn list_sessions() -> ExitCode {
+fn list_sessions(socket_dirs: &[Dir]) -> ExitCode {
     println!("list_sessions");
     ExitCode::SUCCESS
 }
@@ -39,9 +62,15 @@ fn main() -> ExitCode {
     let _program = args.next().expect("program name");
     let mut args = args.collect::<Vec<_>>();
     args.reverse();
+    let socket_dirs: [Dir; 4] = [
+        Dir::form_env("ABDUCO_SOCKET_DIR", false),
+        Dir::form_env("HOME", true),
+        Dir::form_env("TMPDIR", false),
+        Dir::from_path("/tmp", false),
+    ];
 
     if args.is_empty() {
-        return list_sessions();
+        return list_sessions(&socket_dirs);
     }
 
     let mut action = ' ';
@@ -56,7 +85,7 @@ fn main() -> ExitCode {
         if !arg.starts_with("-") {
             if handle_e {
                 let mut chars = arg.chars();
-                if let Some(x) = chars.next()  {
+                if let Some(x) = chars.next() {
                     if x == '^' {
                         if let Some(x) = chars.next() {
                             key_detach = CTRL!(x);
